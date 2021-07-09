f820e0fd58b2e3c0d9c5f8fe5e040b5f6596bf57
Ari Tserendavaa <aritsere@fb.com>
2021-07-08 18:05:12 -0700

    fix

--- udemy/blog/src/actions/index.js
+++ udemy/blog/src/actions/index.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
-import jsonPlaceholder from '../apis/jsonPlaceHolder';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-    await dispatch(fetchPosts());
+  await dispatch(fetchPosts());
 
-    _.chain(getState().posts)
-        .map('userId')
-        .uniq()
-        .forEach(id => dispatch(fetchUser(id)))
-        .value();
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 }
 
 export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
-    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  const response = await jsonPlaceholder.get('/posts');
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
 export const fetchUser = id => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: 'FETCH_USER', payload: response.data });
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
 };
 
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
--- udemy/blog/src/components/UserHeader.js
+++ udemy/blog/src/components/UserHeader.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class UserHeader extends React.Component {
-    render() {
-        const { user } = this.props;
+  render() {
+    const { user } = this.props;
 
-        if (!user) { return null; }
-        return <div className="header">{user.name}</div>;
+    if (!user) {
+      return null;
     }
+
+    return <div className="header">{user.name}</div>;
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return { user: state.users.find(u => u.id === ownProps.userId) };
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps)(UserHeader);
